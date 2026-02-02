10c735045c0cac98d0d0f311718349144f5e7dd9
pratibha <>
2026-02-02 15:20:08 +0530

    Java , Javascript , html and css

--- JAVA/Basic.java
+++ JAVA/Basic.java
@@ -1,5 +1,8 @@
 //  basic program in java that is hello world
 
+// Java :- Java is a programming language which is used to moile application and in development areas .
+// It is a high -level object-oriented language used to build large ,secure and scalable applications . It is platform independent.
+
 
 public class Basic{
     public static void main(String[] args) {
---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,10 @@
+// Javascript (js) is a programming laguage which is used to make website attractive.It is a scripting language which is used to make dynamic content on web pages.
+
+// Javascript can be written in three ways :-                           
+// 1. Inside an html tag
+// 2. Internal javascript with the help of script tag 
+// 3. External javascript , a javascript file is made externally with the help of .js extension
+
+
+
+console.log("Welcome back");
