e98fc081a2d26a5aad8d42e57f433e7a416b3e6d
pratibha <>
2026-02-02 14:56:11 +0530

    java

---
+++ JAVA/Subtract.java
@@ -0,0 +1,9 @@
+public class Subtract{
+    public static void main(String [] args){
+        int a = 10;
+        int b = 7;
+        int sub = a-b;
+
+        System.out.println( "sub =" +sub);
+    }
+}
