6ea1e228ef2318b556f43766ddbdf4fbd2da257d
pratibha <>
2026-02-03 12:46:21 +0530

    variables

---
+++ JAVA/Var.java
@@ -0,0 +1,9 @@
+public class Var{
+    public static void main(String[] args){
+        int a =199;
+        int b =999;
+        int c =a+b;
+
+        System.out.println("c =" +c);
+    }
+}
--- JAVA/Variaariales.java
+++
@@ -1,9 +0,0 @@
-public class Variales{
-    public static void main(String[] args){
-        int a =199;
-        int b =200;
-        int sum = a+b;
-
-        System.out.println
-    }
-}
