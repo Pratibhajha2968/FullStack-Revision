01d45c2e19cbd62f7bf7a3cfdd5f8dc40faf369d
pratibha <>
2026-02-03 12:55:08 +0530

    types of variables

--- JAVA/Var.java
+++ JAVA/Var.java
@@ -7,3 +7,11 @@ public class Var{
         System.out.println("c =" +c);
     }
 }
+
+
+// Variables in java :- variables in java are the container which is used to store datas or values later which can be used or changed during the program execution.With  variables it makes program readable , reusable and dynamic.
+
+// Types of variables :- 
+// 1. Local variables = It is declared inside a method.
+// 2. Instance Variables = Declared inside a class , outside methods.
+// 3. Static Variables = Share among all objects.
