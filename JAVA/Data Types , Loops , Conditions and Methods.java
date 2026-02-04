7d7d251062bcecd9fde893ea61697e40270e31de
pratibha <119747499+Pratibhajha2968@users.noreply.github.com>
2026-02-04 18:17:20 +0530

    datatypes and loops

---
+++ JAVA/Data Types , Loops , Conditions and Methods.java
@@ -0,0 +1,48 @@
+1. DataTypes :- DataTypes in java describe what type of data a variable can store.
+
+ DataTypes are of two types :-
+    1.Primitive DataTypes
+       a.char
+       b.long
+       c.int 
+       d.double
+       e.float
+       f.boolean
+       g.short
+       h.byte 
+    2.Non Primitive DataTypes
+        a.String
+        b.array
+        c.object
+        d.interface
+        e.class   
+
+
+2.   Loops in java :-loops in java are used for iterating a number. We cant write each and everything again and again so we used loops.
+
+   OR In technical word we can say loops are used to execute a block of code repeatedly until the required condition is satisfied.
+
+   Types of loops :-
+       1. for loop 
+          => used when number of counts are known.
+          ex: for(i=0;i<=10;i++)
+       2. foreach loop
+          => it is the modern or enhanced loops which are used mostly .it is used in arrays and other methods.
+          ex: int arr[] ={1,2,3,};
+               for(int num : arr){
+                  syso;
+                  i++;
+               }
+       3. do-while loop
+         => used for atleast one iteration
+         ex: int i=6;
+         do(
+            syso;
+            i++;
+         )while(i<=8)
+       4. while loop
+           => used when number of counts are not known
+          ex: int i=3;
+          while(i<=5)
+            syso;
+            i++;
---
+++ JAVASCRIPT/DataTypes,Loops,Conditions ana methods.js
@@ -0,0 +1,30 @@
+1. DataTypes :- DataTypes tells which type of data a variable can store.
+
+      DataTypes are of two types:- 
+    1.Primitive DataTypes
+       a.Num 
+       b.String
+       c.boolean
+       d.double
+       e. Null
+       f.bigint
+    2.Non primitive DataTypes
+       a.object
+       b.array 
+       c.function 
+       d.Date , Map set etc.
+
+
+2.  Loops in js :- Loops are used to execute a block of code  repeatedly until its meet its required condition.
+
+    Types of loops are :-
+       1.for loop
+         => used when number of counts are known 
+       2.while loop
+         => used when number of counts are not known
+       3.do while loop
+        => used to run atleast once 
+       4.for-of loop
+        => it is the modern or enhanced loop which is used the most for arrays,strings and iterable objects.
+       5.for-in loop
+         => it is used for object.it iterates over keys.
