520eda59ebccc2393d917648e3cd709fb077c9f2
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-05 00:51:44 +0530

    max and eve

---
+++ JAVA/Even.java
@@ -0,0 +1,15 @@
+public class Even{
+    public static void main(String[] args){
+        int num =100;
+        int even = 0;
+
+        for(i=0;i<=100;i++){
+            if(int num%2==0){
+                System.out.println("even number");
+            }else{
+                System.out.println("not even number");
+            }
+        }
+        System.out.println("even");
+    }
+}
---
+++ JAVA/Max.java
@@ -0,0 +1,19 @@
+public class Max{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 89;
+        int c = 23;
+
+        int num=0;
+
+        if(a==b){
+            System.out.println("b is greater");
+        } else (b==c){
+            System.out.println("b is greater");
+        } else-if(a==c){
+            System.out.println("c is greater");
+        }
+
+        System.out.println("num");
+    }
+}
