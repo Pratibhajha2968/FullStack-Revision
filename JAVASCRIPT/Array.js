291a8dbccd8e1c56c6391d2e9a220984ea72a838
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-09 18:08:07 +0530

    arrays in js

---
+++ JAVASCRIPT/Array.js
@@ -0,0 +1,31 @@
+// Arrays in javascript : - An array is a special object used to store different values in a single variable. It is index based and index starts from 0.it is alays ordered and store different datatypes and they are dynamic in size.
+
+// ex :- let numbers = [10,20,30,40,50];
+
+// Array index 0 based :-
+let arr = ["HTML", "CSS", "JAVASCRIPT"];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+
+/* Types of Array in js :-
+          one-dimensional array =
+                  =>Stores values in a single line
+                       let marks = [10,20,30];
+          two-dimensional array  =
+                 => used to store arrays inside array in row and columns like matrix.
+                        let matrix = [
+                                  [10 , 20]
+                                  [30 , 40]
+                                        ];    
+                      
+           jagged array or irregular array or multidimensional array =           
+                   => it is also same as 2d array but the length of inner arrays that is row and coloumn have different lengths.
+                          let matrix =[
+                                 [10,20,30]
+                                 [40,50]
+                                 [60];
+                          ];
+                      
+                      
+                      */
