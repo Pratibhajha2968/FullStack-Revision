aaa50e628c51159bad2634e4c1aa6369c7ff5c14
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-10 01:43:53 +0530

    Strings in java

---
+++ JAVA/String.java
@@ -0,0 +1,21 @@
+public class String {
+    
+}
+/*
+ * String :- String is a sequence of characters used to represent texts.
+                  Strings are always indicated in double quote.
+  
+         Strings in java :- string is an object and it is immutable.
+             String name :-"pratibha"
+  
+ * How Strings are created in java ?
+                       1.using String literal (String pool)
+                                   String s1 = "Hello";
+                                   String s2 = "Hello";
+                           Both are stored in same memory allocation that is String pool.
+
+                       2.using new keyword
+                                String s3 = new String("Hello");    
+                    
+  
+ */ 
