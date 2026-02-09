c02eccf5e55abcaee6f8fed1781d493ad069c0bd
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-10 01:46:40 +0530

    Strings.js

---
+++ JAVASCRIPT/String.js
@@ -0,0 +1,21 @@
+/*
+     Strings in js :- 
+        Strings are primitive data types ut behave like objects.
+           let name ="pratibha";
+
+
+           Strings creation in js:-
+               let s1 = "Hello";
+               let s2 = 'Hello';
+               let s3 = `Hello`; // template literal
+
+
+
+
+
+
+
+
+
+
+*/
