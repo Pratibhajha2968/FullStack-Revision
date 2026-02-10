9faa79d315455b3ed11bb7d76615c35a8a516c87
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-10 15:32:31 +0530

    evenodd

---
+++ JAVA/Array Programs/EvenOdd.java
@@ -0,0 +1,17 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int arr[] = { 90, 34, 22, 33 };
+        int even = arr[0];
+        int odd = arr[0];
+
+        for (int num : arr)
+            if (num % 2 == 0) {
+                System.out.println("even =" + even);
+                
+            } else {
+                System.out.println("odd =" + odd);
+                odd++;
+            }
+    }
+
+}
--- JAVA/Array Programs/Reverse.java
+++ JAVA/Array Programs/Reverse.java
@@ -2,7 +2,7 @@ public class Reverse {
     public static void main(String[] args) {
         int arr[] = { 11, 22, 33, 44, 55, 66 };
         // int reverse = arr[0];
-        for (int i = 0; i < arr.length - 1; i--) {
+        for (int i = arr.length - 1; i >= 0 ; i--) {
             System.out.println(arr[i] + "");
         }
 
