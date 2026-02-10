bfbc69d0961fb61f8801dda774967b0856de34eb
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-10 15:16:59 +0530

    reverse

---
+++ JAVA/Array Programs/Reverse.java
@@ -0,0 +1,11 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int arr[] = { 11, 22, 33, 44, 55, 66 };
+        // int reverse = arr[0];
+        for (int i = 0; i < arr.length - 1; i--) {
+            System.out.println(arr[i] + "");
+        }
+
+    }
+
+}
