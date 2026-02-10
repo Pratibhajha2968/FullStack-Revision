a87cfd5396c9486f20c03beba35f1a8ec217b1fb
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-10 15:48:54 +0530

    sum

--- JAVA/Array Programs/EvenOdd.java
+++ JAVA/Array Programs/EvenOdd.java
@@ -2,7 +2,7 @@ public class EvenOdd {
     public static void main(String[] args) {
         int arr[] = { 90, 34, 22, 33 };
         int even = arr[0];
-        int odd = arr[0];
+        int odd = arr[3];
 
         for (int num : arr)
             if (num % 2 == 0) {
