e5f3c9d51bbeff5dac472fb42d0278d7db914f98
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-13 00:00:06 +0530

    update

--- REACT/React Project/src/states/State.jsx
+++ REACT/React Project/src/states/State.jsx
@@ -3,6 +3,10 @@
                State refers to the values stored in variable at a particular time.
 
 
+               int count = 0;   // state = 0
+                 count = 5;    // state changed to 5
+
+
 
 
 
