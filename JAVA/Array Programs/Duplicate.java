979d36ac77e5d51f6bf84825704619640c849f8f
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-12 23:56:51 +0530

    states in react

--- JAVA/Array Programs/Duplicate.java
+++ JAVA/Array Programs/Duplicate.java
@@ -6,7 +6,7 @@ public class Duplicate {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; i++) {
                 if (arr[i] == arr[j]) {
-                    System.out.println("Duplicate: " + arr[j]);
+                    System.out.println("Duplicate:]]]]] arr[j]);
                 }
             }
         }
---
+++ REACT/React Project/src/states/State.jsx
@@ -0,0 +1,10 @@
+/*
+   States in React :- State  means data or condition of something that can change over a period of time.
+               State refers to the values stored in variable at a particular time.
+
+
+
+
+
+
+*/
