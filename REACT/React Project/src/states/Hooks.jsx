7f25773829bb2c5591caa6c9a747bca8af165be4
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-14 01:44:26 +0530

    types of hooks

--- REACT/React Project/src/states/Hooks.jsx
+++ REACT/React Project/src/states/Hooks.jsx
@@ -7,7 +7,14 @@
 
          hooks are inbuilt function components in react
    -     can use hooks inside function component on top of other code like conditional
-         ,looping and rendering etc.,    
+         ,looping and rendering etc.,
+         
+
+         Types:
+-------
+    1.inbuilt hooks- inbuilt /existing hooks
+    2.custom hooks - created by developer/user
+
 
 
 */
