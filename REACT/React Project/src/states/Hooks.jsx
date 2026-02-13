984004ea2f0a16f36be9b07a5d55832de625af17
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-14 01:40:59 +0530

    react hooks

---
+++ REACT/React Project/src/states/Hooks.jsx
@@ -0,0 +1,11 @@
+/*
+        Hooks :- React hooks are special functions that allows you to use state and other react features.
+                 Hooks are used to manage state and lifecycle.
+                 Hooks are inbuilt in react 
+                 hooks were introduced in 16.8 version
+
+
+                 
+
+
+*/
