fc90a45ebe6f7a77f52281583c3cbe32ddd563e4
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-20 16:58:16 +0530

    Basic Hooks

--- REACT/React Project/src/states/Hooks.jsx
+++ REACT/React Project/src/states/Hooks.jsx
@@ -18,5 +18,13 @@
 
     React Hooks are special functions introduced in React 16.8 that let you use state and other React features in functional components. Hooks allow functional components to do things that previously only class components could do, like managing state or using lifecycle methods.
 
+                    Types of hooks in react are :-
+                       1. Basic Hooks :-
+                          This are the most common used hooks in every project.
+                            => useState =It is used to manage local state.
+                            => useEffect =It is used to call or handle side effects    (API calls , timers etc)
+                            => useContext =It is used to handle or access global data from context.
+                    All this hooks are very important in day to day project.
+
 
 */
