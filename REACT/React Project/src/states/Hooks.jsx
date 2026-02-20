71986c2fd2a59a07d493dc40ae4bfed19f852ad0
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-20 18:37:46 +0530

    First ReactHook

--- REACT/React Project/src/states/Hooks.jsx
+++ REACT/React Project/src/states/Hooks.jsx
@@ -18,13 +18,27 @@
 
     React Hooks are special functions introduced in React 16.8 that let you use state and other React features in functional components. Hooks allow functional components to do things that previously only class components could do, like managing state or using lifecycle methods.
 
-                    Types of hooks in react are :-
-                       1. Basic Hooks :-
-                          This are the most common used hooks in every project.
-                            => useState =It is used to manage local state.
-                            => useEffect =It is used to call or handle side effects    (API calls , timers etc)
-                            => useContext =It is used to handle or access global data from context.
+        Types of hooks in react are :-
+          1. Basic Hooks :-
+             This are the most common used hooks in every project.
+                => useState =It is used to manage local state inside a functional component.
+                  When we update state:
+                    1.React stores the new values.
+                    2.React re-renders the components. 
+                    3.UI updates automatically.
+                         State updates are asynchronous.
+                         Never mutate state directly.
+                         Functional update pattern:
+                               Counters
+                               Form inputs
+                               Toggle buttons
+                               UI visibility
+                => useEffect =It is used to call or handle side effects    (API calls , timers etc)
+                => useContext =It is used to handle or access global data from context.
                     All this hooks are very important in day to day project.
+
+                       
+                          
 
 
 */
