709087ff0cbe3687361edba81b519bae4d6f57f0
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-26 15:33:34 +0530

    oops

---
+++ JAVA/oops/OOps.java
@@ -0,0 +1,8 @@
+/*
+    What is OOPs ?
+       OOPs stands for Object Oriented Programming language . It is a programming paradigm based on objects and classes. It helps in organizing code using real world concepts .
+
+
+
+
+*/
