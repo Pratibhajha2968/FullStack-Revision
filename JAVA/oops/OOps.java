1877121d44d5010827813c6163561b19dff23771
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-26 17:55:36 +0530

    encapsulation

--- JAVA/oops/OOps.java
+++ JAVA/oops/OOps.java
@@ -1,27 +1,40 @@
 /*
-       What is OOPs ?
-            OOPs stands for Object Oriented Programming language . It is a programming
-            paradigm based on objects and classes. It helps in organizing code using real world concepts .
-  
-             4 pillars of oops ?
-                 1. Abstraction :-
-                   Hiding internal implementation details and showing only essential features.
-                   User doesnot know how something works internally .
-                    How abstraction can be achieved :-
-                         1. Abstract classes.
-                         2. Interface.
-  
-                  Making data members private prevents direct access.
-                  Setters do not break security because they allow controlled access where we can validate and restrict input.
-                  Instead of exposing variables, we expose behavior.
-                  In secure systems, we sometimes avoid setters and use immutable classes.
-
-                  
-                 2. Encapsulation
-                 3. Inheritance
-                 4. Polymorphism
-  
-  
- 
- 
+ *        What is OOPs ?
+ * OOPs stands for Object Oriented Programming language . It is a programming
+ * paradigm based on objects and classes. It helps in organizing code using real
+ * world concepts .
+ * 
+ *       4 pillars of oops ?
+ * 1. Abstraction :-
+ * Hiding internal implementation details and showing only essential features.
+ * User doesnot know how something works internally .
+ * How abstraction can be achieved :-
+ * 1. Abstract classes.
+ * 2. Interface.
+ * 
+ * Making data members private prevents direct access.
+ * Setters do not break security because they allow controlled access where we
+ * can validate and restrict input.
+ * Instead of exposing variables, we expose behavior.
+ * In secure systems, we sometimes avoid setters and use immutable classes.
+ * 
+ * An abstract class is used when classes are closely related and share common
+ * logic. It can have both abstract and concrete methods and constructors.
+ * An interface is used to define a contract that unrelated classes can
+ * implement. It supports multiple inheritance and provides full abstraction.
+ * 
+ * 
+ *    2. Encapsulation :-
+           Wrapping or binding up of data members (variables) and methods in a single  class is called Encapsulation .
+           We can achieve encapsulation by :-
+               1. Private (variables)
+               2. public(getter and setter methods)
+               
+            We use encapsulation to protects data , unautorized access and prevents security.
+ * 3. Inheritance
+ * 4. Polymorphism
+ * 
+ * 
+ * 
+ * 
  */
