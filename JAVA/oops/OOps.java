32fc3b79669f781ed4de07fce047ea853f12faee
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-26 16:26:52 +0530

    encapsilation

--- JAVA/oops/OOps.java
+++ JAVA/oops/OOps.java
@@ -1,18 +1,27 @@
 /*
-    What is OOPs ?
-       OOPs stands for Object Oriented Programming language . It is a programming paradigm based on objects and classes. It helps in organizing code using real world concepts .
+       What is OOPs ?
+            OOPs stands for Object Oriented Programming language . It is a programming
+            paradigm based on objects and classes. It helps in organizing code using real world concepts .
+  
+             4 pillars of oops ?
+                 1. Abstraction :-
+                   Hiding internal implementation details and showing only essential features.
+                   User doesnot know how something works internally .
+                    How abstraction can be achieved :-
+                         1. Abstract classes.
+                         2. Interface.
+  
+                  Making data members private prevents direct access.
+                  Setters do not break security because they allow controlled access where we can validate and restrict input.
+                  Instead of exposing variables, we expose behavior.
+                  In secure systems, we sometimes avoid setters and use immutable classes.
 
-    4 pillars of oops ?
-         1. Abstraction :-
-                Hiding internal implementation details and showing only essential features. User doesnot know how something works internally .
-                      How abstraction can be achieved :-
-                           1. Abstract classes.
-                           2. Interface.
-         2. Encapsulation
-         3. Inheritance
-         4. Polymorphism   
-
-
-
-
-*/
+                  
+                 2. Encapsulation
+                 3. Inheritance
+                 4. Polymorphism
+  
+  
+ 
+ 
+ */
