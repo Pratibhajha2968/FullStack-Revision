41ba8d812f06eded4f3e5aa71038445c0e8b8c43
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-02-26 15:39:03 +0530

    pillars of oops

--- JAVA/oops/OOps.java
+++ JAVA/oops/OOps.java
@@ -2,6 +2,13 @@
     What is OOPs ?
        OOPs stands for Object Oriented Programming language . It is a programming paradigm based on objects and classes. It helps in organizing code using real world concepts .
 
+    4 pillars of oops ?
+         1. Abstraction :-
+                
+         2. Encapsulation
+         3. Abstraction
+         4. Polymorphism   
+
 
 
 
