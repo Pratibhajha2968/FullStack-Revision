2162899fda1e35506b6ac983bfafefcdcbfc2257
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-03-01 01:44:41 +0530

    closures

--- JAVASCRIPT/Closures.js
+++ JAVASCRIPT/Closures.js
@@ -3,6 +3,11 @@
       => A closure is created when:
            A function remembers variables from its outer scope even after the outer function has finished execution.
 
+        Why Important?
+             Used in data privacy
+             Used in React (state logic understanding)
+             Used in callbacks
+
 
 
 */
