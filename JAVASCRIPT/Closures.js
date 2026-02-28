e94ec957c738814c82880bb51c8273333bc6c8d0
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-03-01 01:38:07 +0530

    closures in js

---
+++ JAVASCRIPT/Closures.js
@@ -0,0 +1,8 @@
+/*   
+     What is closures ?
+      => A closure is created when:
+           A function remembers variables from its outer scope even after the outer function has finished execution.
+
+
+
+*/
