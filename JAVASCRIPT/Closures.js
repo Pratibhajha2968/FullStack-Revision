f79498238b77bb1a44e87fbed59d226290ab2a6a
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-03-01 01:48:46 +0530

    promises

--- JAVASCRIPT/Closures.js
+++ JAVASCRIPT/Closures.js
@@ -8,6 +8,11 @@
              Used in React (state logic understanding)
              Used in callbacks
 
+=====================================================================================
+
+     What are promises ? 
+      ==> A promise represents a value that may be available now , later or never .
+
 
 
 */
