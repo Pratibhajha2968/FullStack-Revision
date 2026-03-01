69c580c6b2ffe78c4d31c29e434ebb8cfe751edd
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-03-02 00:30:56 +0530

    props vs state

--- REACT/React Project/src/Props/props.jsx
+++ REACT/React Project/src/Props/props.jsx
@@ -2,6 +2,8 @@
    Props in React :-
         Props are used to pass data from a parent component to a child component. They are read-only and cannot be modified by the child component. 
 
+        The key difference is that state is managed inside the component and can be updated, while props are passed from outside and are immutable inside the receiving component.
+
 
 
 
