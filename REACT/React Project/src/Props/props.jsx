eee131c743e3a838da7a0fd38e22ccb14382ed3a
Pratibhajha2968 <pratibhajha296@gmail.com>
2026-03-02 00:27:55 +0530

    props

---
+++ REACT/React Project/src/Props/props.jsx
@@ -0,0 +1,8 @@
+/*
+   Props in React :-
+        Props are used to pass data from a parent component to a child component. They are read-only and cannot be modified by the child component. 
+
+
+
+
+*/
